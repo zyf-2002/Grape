d91dd8474b10a6ed16427190fd39c80e3546a04d
zyf-2002 <1525871396@qq.com>
2025-12-05 17:47:03 +0800

    bash

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 mkdir depends && cd depends
 git init
 git submodule add https://github.com/scipr-lab/libsnark.git libsnark
-
+git submodule update --init --recursive
 sudo apt-get install build-essential cmake git libgmp3-dev libprocps4-dev python-markdown libboost-all-dev libssl-dev pkg-config
 
 
@@ -9,5 +10,5 @@ cd ..
 mkdir build && cd build
 cmake ..
 make -j$(nproc)
-./HyraxTest
+
 
